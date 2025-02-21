6bd2a9418223b8d051660a0144141ba127ad3b23
lucas <82159664+lucasV47@users.noreply.github.com>
2025-02-21 11:47:58 -0300

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,40 +1,70 @@
 import java.util.Scanner;
 
 public class Calculator {
+
+    static double num1;
+    static double num2;
+    static char operator;
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean continueCalculating = true;
 
-//        get input numbers from user
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("enter 2 numbers");
-        double num1= scanner.nextDouble();
-        double num2= scanner.nextDouble();
+        while (continueCalculating) {
+            // get input numbers from user
+            System.out.println("Enter 2 numbers:");
+            while (true) {
+                if (sc.hasNextDouble()) {
+                    num1 = sc.nextDouble();
+                    if (sc.hasNextDouble()) {
+                        num2 = sc.nextDouble();
+                        break; // exit loop if both numbers are valid
+                    } else {
+                        System.out.println("Please enter a valid second number.");
+                        sc.next(); // clear the invalid input
+                    }
+                } else {
+                    System.out.println("Please enter a valid first number.");
+                    sc.next(); // clear the invalid input
+                }
+            }
+            // What operation to perform
+            System.out.println("What operation would you like to perform? (+, -, *, /)");
+            while (true) {
+                operator = sc.next().charAt(0);
+                switch (operator) {
+                    case '+':
+                        System.out.println("Addition result: " + (num1 + num2));
+                        break;
+                    case '-':
+                        System.out.println("Subtraction result: " + (num1 - num2));
+                        break;
+                    case '*':
+                        System.out.println("Multiplication result: " + (num1 * num2));
+                        break;
+                    case '/':
+                        if (num2 != 0) {
+                            System.out.println("Division result: " + (num1 / num2));
+                        } else {
+                            System.out.println("Error: Division by zero is not allowed.");
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid operator. Please enter one of (+, -, *, /):");
+                        continue; // ask for operator again
+                }
+                break; // exit the operator loop if a valid operator was processed
+            }
 
-//    what operation would you like to perform? (+, -, *, /, )
-        System.out.println("what operation would you like to perform? (+, -, *, /, )");
-        char operator = scanner.next().charAt(0);
-
-        switch (operator){
-            case '+':
-                System.out.println("addition result: "+ (num1 + num2) );
-                break;
-
-            case '-':
-                System.out.println("subtraction result is: "+ (num1 - num2 ));
-                break;
-            case '*':
-                System.out.println("multiplication result is: "+ (num1 * num2 ));
-                break;
-            case '/':
-                if (num2 != 0) {
-                    System.out.println("division result is: " + (num1 / num2));
-                    break;}
-
-                else{
-                    System.out.println("error, please enter a valid number. ");
-                    break;}
-
+            // ask if the user wants to perform another calculation
+            System.out.println("Would you like to make another calculation? (y/n)");
+            String response = sc.next();
+            if (!response.equalsIgnoreCase("y")) {
+                continueCalculating = false;
+            }
         }
 
-    }}
-
-
+        sc.close(); // close the scanner
+        System.out.println("Calculator closed.");
+    }
+}
